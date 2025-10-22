bffdfaab068fbe65a0787051469c4f483e5ac039
Azam Khan <azamkhan561@gmail.com>
2025-10-22 08:38:46 +0530

    Added zod validator

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,3 +1,4 @@
+import { Product } from "@/types";
 import ProductCard from "./product-card";
 
 const ProductList = ({
@@ -5,7 +6,7 @@ const ProductList = ({
   title,
   limit,
 }: {
-  data: any;
+  data: Product[];
   title?: string;
   limit?: number;
 }) => {
@@ -15,7 +16,7 @@ const ProductList = ({
       <h2 className="h2-bold mb-4">{title}</h2>
       {data?.length ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-          {limitedData?.map((product: any) => (
+          {limitedData?.map((product: Product) => (
             <ProductCard product={product} key={product?.slug} />
           ))}
         </div>
